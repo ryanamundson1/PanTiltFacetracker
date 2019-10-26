9ab05728744a86c376192c905469e7d4ca7691e4
Ryan Amundson <ryanamundson1@yahoo.com>
2019-10-26 16:05:30 -0500

    Update and rename facetracker_lbp.py to facetracker.py

--- facetracker.py
+++ facetracker.py
@@ -3,25 +3,29 @@
 import cv2, sys, time, os
 from pantilthat import *
 
-# Load the BCM V4l2 driver for /dev/video0
-os.system('sudo modprobe bcm2835-v4l2')
-# Set the framerate ( not sure this does anything! )
-os.system('v4l2-ctl -p 8')
 
 # Frame Size. Smaller is faster, but less accurate.
 # Wide and short is better, since moving your head
 # vertically is kinda hard!
-FRAME_W = 180
-FRAME_H = 100
+FRAME_W = 320
+FRAME_H = 200
 
 # Default Pan/Tilt for the camera in degrees.
 # Camera range is from -90 to 90
 cam_pan = 90
 cam_tilt = 90
 
+current_pan = 0
+current_tilt = 0
+
+# Load the BCM V4l2 driver for /dev/video0
+os.system('sudo modprobe bcm2835-v4l2')
+# Set the framerate ( not sure this does anything! )
+os.system('v4l2-ctl -p 8')
+
 # Set up the CascadeClassifier for face tracking
-#cascPath = '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml' # sys.argv[1]
-cascPath = '/usr/share/opencv/haarcascades/haarcascade_eye.xml' 
+cascPath = '/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml' # sys.argv[1]
+#cascPath = '/usr/share/opencv/haarcascades/haarcascade_eye.xml' 
 #cascPath = '/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
 
@@ -32,8 +36,8 @@ video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_H)
 time.sleep(2)
 
 # Turn the camera to the default position
-pan(cam_pan-90)
-tilt(cam_tilt-90)
+pan(current_pan)
+tilt(current_tilt)
 light_mode(WS2812)
 
 def lights(r,g,b,w):
@@ -41,7 +45,7 @@ def lights(r,g,b,w):
         set_pixel_rgbw(x,r if x in [3,4] else 0,g if x in [3,4] else 0,b,w if x in [0,1,6,7] else 0)
     show()
 
-lights(0,0,0,50)
+#lights(0,0,0,50)
 
 while True:
     # Capture frame-by-frame
@@ -58,20 +62,30 @@ while True:
     gray = cv2.equalizeHist( gray )
 
     # Do face detection
-    faces = faceCascade.detectMultiScale(frame, 1.1, 3, 0, (5, 5))
+    #faces = faceCascade.detectMultiScale(frame, 1.1, 3, 0, (5, 5))
    
     # Slower method 
-    '''faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=4,
-        minSize=(20, 20),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE | cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT | cv2.cv.CV_HAAR_DO_ROUGH_SEARCH
-    )'''
+    faces = list(faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(20,20)
+            #flags = cv2.CASCADE_SCALE_IMAGE
+        ))
     
-    lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
+    #lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
 
-    for (x, y, w, h) in faces:
+    face = None
+    
+    if faces:
+        face = faces[0]
+        
+        faceFound = True
+        
+        faceTime = datetime.now() #- timedelta(days=N)
+    
+        x, y, w, h = face
+        
         # Draw a green rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
@@ -92,24 +106,41 @@ while True:
         # Scale offset to degrees
         turn_x   *= 2.5 # VFOV
         turn_y   *= 2.5 # HFOV
+        
+        #print (turn_x, turn_y)
         #cam_pan  += -turn_x
         #cam_tilt += turn_y
         
         #Cam seemed to be turning away, switch this
-        cam_pan  += turn_x
-        cam_tilt += -turn_y
+        current_pan += turn_x
+        current_pan += turn_y
 
-        print(cam_pan-90, cam_tilt-90)
+        #print(cam_pan-90, cam_tilt-90)
 
         # Clamp Pan/Tilt to 0 to 180 degrees
-        cam_pan = max(0,min(180,cam_pan))
-        cam_tilt = max(0,min(180,cam_tilt))
+        current_pan = max(-90,min(90, current_pan))
+        current_pan = max(-90,min(90, current_pan))
+        
+        print(current_pan, current_pan)
 
         # Update the servos
-        pan(int(cam_pan-90))
-        tilt(int(cam_tilt-90))
+        pan(int(current_pan))
+        tilt(int(current_pan))
+    
+    else:
+        faceFound = False
+        currenttime = datetime.now()
+        
+        if faceTime:
+            
+            endtime = faceTime + timedelta(seconds=5)
 
-        break
+            if currenttime > endtime:
+                # if we havent found faces for 5 seconds, reset the pan
+                current_pan = 0
+                current_tilt = 0
+                pan(current_pan)
+                tilt(current_tilt)
 
     frame = cv2.resize(frame, (540,300))
     frame = cv2.flip(frame, 1)
